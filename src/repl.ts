da8d58426b9633307b309cdb8c4aa07d0e7dabed
raknage <raknage@raknage.com>
2025-08-04 12:08:45 +0300

    CH1 L4 REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,8 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const prompt = "POKEDEX > ";
+
 export function cleanInput(input: string): string[] {
   const arr = input
     .toLowerCase()
@@ -7,4 +12,22 @@ export function cleanInput(input: string): string[] {
       return e != "";
     });
   return arr;
+}
+
+export function startREPL() {
+  const readlineInterace = readline.createInterface({
+    input,
+    output,
+    prompt,
+  });
+  readlineInterace.prompt();
+  readlineInterace.on("line", (line) => {
+    const cleanArr = cleanInput(line);
+    if (cleanArr.length === 0) {
+      readlineInterace.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleanArr[0]}`);
+    readlineInterace.prompt();
+  });
 }
