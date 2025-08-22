68d011d26f5ea697f0e2786897e95816f44a6009
raknage <raknage@raknage.com>
2025-08-22 14:23:31 +0300

    update App component to handle commands and  CLI rendering

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
-import {Text} from 'ink';
+// import React from "react";
+import { useState, useEffect } from "react";
+import { initState, State, prompt } from "../core/state.js";
+import { cleanInput } from "../core/repl.js";
+import { Text, Box, useFocus } from "ink";
+import TextInput from "ink-text-input";
 
-type Props = {
-	name: string | undefined;
-};
+export default function App() {
+  const [state, setState] = useState<State | null>(null);
+  const [output, setOutput] = useState<string[]>([]);
+  const [input, setInput] = useState<string>("");
+  useFocus();
 
-export default function App({name = 'Stranger'}: Props) {
-	return (
-		<Text>
-			Hello, <Text color="green">{name}</Text>
-		</Text>
-	);
+  useEffect(() => {
+    const init = async () => {
+      setState(await initState(1000 * 60 * 60));
+      setOutput([]);
+    };
+
+    init();
+  }, []);
+
+  async function handleCommand(input: string) {
+    if (input === "") {
+      // console.log(output);
+      setOutput((prevOutput) => [...prevOutput, prompt]);
+      return;
+    }
+
+    const args = cleanInput(input);
+    const [commandName, ...commandArgs] = args;
+
+    // Ensure that state can't be null here to satisfy TS
+    if (!state) {
+      setOutput((prevOutput) => [...prevOutput, "State not initialized"]);
+      return;
+    }
+    const command = state.commands[commandName];
+
+    if (command === undefined) {
+      setOutput((prevOutput) => [...prevOutput, "Unknown command"]);
+      setInput("");
+      return;
+    }
+
+    let cmdOutput = "";
+
+    try {
+      cmdOutput = await command.callback(state, ...commandArgs);
+    } catch (e) {
+      setOutput((prevOutput) => [...prevOutput, `${(e as Error).message}`]);
+      return;
+    }
+
+    setInput("");
+    setOutput((prevOutput) => [...prevOutput, cmdOutput]);
+  }
+
+  return (
+    <>
+      {output.map((line, index) => (
+        <Text key={index}>{line}</Text>
+      ))}
+      <Box borderStyle="single" borderColor="blue">
+        <Text color="blue">{prompt}</Text>
+        <TextInput value={input} onChange={setInput} onSubmit={handleCommand} />
+      </Box>
+    </>
+  );
 }
--- src/ui/cli.tsx
+++ src/ui/cli.tsx
@@ -1,29 +1,7 @@
 #!/usr/bin/env node
-import React from 'react';
-import {render} from 'ink';
-import meow from 'meow';
-import App from './app.js';
+// import React from "react";
+import { render } from "ink";
+// import meow from "meow";
+import App from "./app.js";
 
-const cli = meow(
-	`
-	Usage
-	  $ pokedex
-
-	Options
-		--name  Your name
-
-	Examples
-	  $ pokedex-ink --name=Jane
-	  Hello, Jane
-`,
-	{
-		importMeta: import.meta,
-		flags: {
-			name: {
-				type: 'string',
-			},
-		},
-	},
-);
-
-render(<App name={cli.flags.name} />);
+render(<App />);
